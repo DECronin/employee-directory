d2b144e8554107d7cbe43074bcbc702a65080695
Dana Cronin <cronin43224235@gmail.com>
2020-02-10 18:32:26 -0500

    rendering array properly // YAY

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
+import API from "../../utils/API";
 import Table from 'react-bootstrap/Table'
 import 'bootstrap/dist/css/bootstrap.min.css';
-const defaultArray = [0, 1, 2, 3, 4];
-// import API from "../../utils/API"
 
 class Directory extends Component {
     state = {
-        employees: defaultArray
+        default: [],
+        currentEmployees: []
     };
     populateRows = () => {
+        console.log(`topPop::\n${JSON.stringify(this.state.currentEmployees)}`);
         let newArray = [];
-        this.state.employees.forEach(i =>{
+        this.state.currentEmployees.forEach(em => {
+            console.log(JSON.stringify(em));
             newArray.push(<tr>
                 {/* id */}
-                <td>{i}</td>
+                <td>{em.index}</td>
                 {/* username */}
-                <td>{i}</td>
+                <td>{em.login.username}</td>
                 {/* name (last, first) */}
-                <td>{i}</td>
+                <td>{em.name.last}, {em.name.first}</td>
                 {/* status */}
-                <td>{i}</td>
+                <td>"Status"</td>
                 {/* dep */}
-                <td>{i}</td>
+                <td>"DEP"</td>
                 {/* location */}
-                <td>{i}</td>
+                <td>{em.location.city}, {em.location.state}</td>
                 {/* phone */}
-                <td>{i}</td>
+                <td>{em.phone}</td>
                 {/* email */}
-                <td>{i}</td>
+                <td>{em.email}</td>
                 {/* age */}
-                <td>{i}</td>
+                <td>{em.dob.age}</td>
             </tr>)
         })
         return newArray
@@ -40,31 +42,38 @@ class Directory extends Component {
     // filter = col => {
     //     this.state.employees = this.state.employees.filter(col)
     // };
-    reset(){
-        this.setState({employees: defaultArray})
-    };
-    render(){
-        // this.populateRows();
+    // reset() {
+    //     this.setState({ currentEmployees: this.state.default })
+    // };
+    componentDidMount() {
+        API.mergeDatabase().then(res => {
+            this.setState({
+                default: res.data.results,
+                currentEmployees: res.data.results
+            })
+        })
+    }
+    render() {
         return (<>
             <Table striped bordered hover variant="dark">
                 <thead>
                     <tr>
                         {/* add reset button */}
                         <th>ID: <button>S</button></th>
-                         {/* onClick={this.sort('id')} */}
+                        {/* onClick={this.sort('id')} */}
                         <th>Username:</th>
                         <th>Name: (Last, First) <button>S</button></th>
-                         {/* onClick={this.sort('lastName')} */}
+                        {/* onClick={this.sort('lastName')} */}
                         <th>Status: <button>F</button></th>
-                         {/* onClick={this.filter('status')} */}
+                        {/* onClick={this.filter('status')} */}
                         <th>Department: <button>F</button></th>
-                         {/* onClick={this.filter('dep')} */}
+                        {/* onClick={this.filter('dep')} */}
                         <th>Location: <button>F</button></th>
-                         {/* onClick={this.filter('location')} */}
+                        {/* onClick={this.filter('location')} */}
                         <th>Phone:</th>
                         <th>Email:</th>
                         <th>Age: <button>S</button></th>
-                         {/* onClick={this.sort('age')} */}
+                        {/* onClick={this.sort('age')} */}
                     </tr>
                 </thead>
                 <tbody>
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function (data) {
-        console.log(data);
-    }
-});
+import axios from "axios";
+
+export default {
+  mergeDatabase: function() {
+    return axios.get("https://randomuser.me/api/?results=25")
+  }
+}
