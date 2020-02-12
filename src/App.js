93a2b6966aa2b619fd9df414fcc24de5e360c062
Dana Cronin <cronin43224235@gmail.com>
2020-02-11 20:40:26 -0500

    FA library

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Wrapper from "./components/Wrapper";
 import Directory from "./components/Directory";
 import Header from "./components/Header";
 import './App.css';
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faFilter, faSortNumericDown, faSortAlphaDown } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faFilter, faSortNumericDown, faSortAlphaDown)
 
 function App() {
   return (
